0ff5aa6466cd092da3cde62c228073ca2721f222
haribo <zhdl453@naver.com>
2024-01-01 13:44:02 +0900

    이그노어 유저세팅

--- Puzzle_ManyKinds/Assets/Scripts/Whack A Mole/Mole.cs
+++ Puzzle_ManyKinds/Assets/Scripts/Whack A Mole/Mole.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Mole : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Graphics")]
+    [SerializeField] private Sprite mole;
+    [SerializeField] private Sprite moleHardHat;
+    [SerializeField] private Sprite moleHatBroken;
+    [SerializeField] private Sprite moleHit;
+    [SerializeField] private Sprite moleHatHit;
+
+    [Header("GameManager")]
+    [SerializeField] private GameManager gameManager;
+
+    //The offset of the sprite to hide it
+    private Vector2 StartPosition = new Vector2(0f, -2.56f);
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
